f340e640fb97b08e77e132e4c2557f2184277ec3
adrian-DC <adrian.lara@datacivica.org>
2022-08-15 01:55:50 -0500

    Visualizacion automatizada inicial

--- explore-enco.R
+++ explore-enco.R
@@ -1,5 +1,5 @@
 require(pacman)
-p_load(tidyverse, foreign, janitor, data.table, cumstats)
+p_load(tidyverse, foreign, janitor, data.table, cumstats, scales)
 
 # Abrimos
 
@@ -27,23 +27,121 @@ clean_socioec <- socioec %>%
   unite("id", per:n_ren) %>% 
   select(id, sex, eda)
 
+rm(socioec)
+# Decodifcación de respuestas:
+
+r1_6 <- function(x){
+  case_when(
+    x == 1 ~ "Mucho mejor", 
+    x == 2 ~ "Mejor" ,
+    x == 3 ~ "Igual",
+    x == 4 ~ "Peor",
+    x == 5 ~  "Mucho peor",
+    x == 6 ~ "No sabe",
+    T ~ "check decoding")
+}
+
+r7 <- function(x){
+  case_when(
+    x == "1" ~ "Sí",
+    x == "2" ~ "Igual posibilidad",
+    x == "3" ~ "No",
+    x == "4" ~ "No sabe",
+    T ~ "check decoding"
+  )
+}
+
+r8 <- function(x){
+  case_when(
+    x == "1" ~ "Mayores",
+    x == "2" ~ "Iguales",
+    x == "3" ~ "Menores",
+    x == "4" ~ "No sabe",
+    T ~ "check decoding"
+  )
+}
+
+r9 <- function(x){
+  case_when(
+    x == "1" ~ "Sí",
+    x == "2" ~ "No",
+    x == "3" ~ "No sabe",
+    T ~ "check decoding"
+    )
+}
+
+r10 <- function(x){
+  case_when(
+    x == "1" ~ "Sí",
+    x == "2" ~ "No",
+    x == "3" ~ "No sabe",
+    x == "4" ~ "No tiene ingresos",
+    T ~ "check decoding"
+  )
+}
+
+r11 <- function(x){
+  case_when(
+    x == "1" ~ "Muy buenas",
+    x == "2" ~ "Buenas",
+    x == "3" ~ "Iguales",
+    x == "4" ~ "Malas",
+    x == "5" ~ "Muy malas",
+    x == "6" ~ "No sabe",
+    T ~ "check decoding"
+  )
+}
+
+r12 <- function(x){
+  case_when(
+    x == "1" ~ "Disminuirán mucho",
+    x == "2" ~ "Disminuirán poco",
+    x == "3" ~ "Permanecerán igual",
+    x == "4" ~ "Aumentarán poco",
+    x == "5" ~ "Aumentarán igual",
+    x == "6" ~ "Aumentarán mucho",
+    x == "7" ~ "No sabe",
+    T ~ "check decoding"
+  )
+}
+
+r13 <- function(x){
+  case_when(
+    x == "1" ~ "Aumentará mucho",
+    x == "2" ~ "Aumentará poco",
+    x == "3" ~ "Permanecerá igual",
+    x == "4" ~ "Disminuirá poco",
+    x == "5" ~ "Disminuirá mucho",
+    x == "6" ~ "No sabe",
+    T ~ "check decoding"
+  )
+}
+
+r14_15 <- function(x){
+  case_when(
+    x == "1" ~ "Sí",
+    x == "2" ~ "Probablemente",
+    x == "3" ~ "No",
+    x == "4" ~ "No sabe",
+    T ~ "check decoding")
+}
+
+
 clean_cuestbas <- cuestbas %>% 
   unite("id", per:n_ren) %>% 
-  mutate(p14 = case_when(
-    p14 == "1" ~ "Sí",
-    p14 == "2" ~ "Probablemente",
-    p14 == "3" ~ "No",
-    p14 == "4" ~ "No sabe"),
-    p10 = case_when(
-      p10 == "1" ~ "Sí",
-      p10 == "2" ~ "No",
-      p10 == "3" ~ "No sabe",
-      p10 == "4" ~ "No tiene ingresos")) %>% 
+  mutate(across(p1:p6, r1_6),
+         p7 = r7(p7), p8 = r8(p8),
+         p9 = r9(p9), p10 = r10(p10),
+         p11 = r11(p11), p12 = r12(p12),
+         p13 = r13(p13),
+         across(p14:p15, r14_15)) %>% 
+  mutate() %>% 
   left_join(., clean_socioec, by = "id") 
 
+rm(cuestbas)
 # Grafs
 
-# Función general para visualizar promedios acumulados 
+# 1.- Función general para preparación de porcentajes y porcentajes promedios 
 
 make_data <- function(db, colname){
   tempo <- db %>% 
@@ -56,33 +154,72 @@ make_data <- function(db, colname){
     ungroup() %>% 
     arrange(across(c(starts_with(colname), eda))) %>%
     group_by(across(c(starts_with(colname)))) %>%
-    mutate(cummean = dplyr::cummean(porcentaje))
+    mutate(cummean = dplyr::cummean(porcentaje)) %>% 
+    pivot_longer(porcentaje:cummean, names_to = "var_stat", 
+                 values_to = "value") %>% 
+    mutate(var_stat = ifelse(var_stat == "porcentaje", "Porcentaje",
+                             "Porcentaje promedio acumulado"))
 }
 
+# Testing 
 
-plot_meanline <- function(colname, title){
+# test <- make_data(clean_cuestbas, "p14") 
+#   
+# 
+# ggplot(test %>% dplyr::filter(get("p14") != "No sabe",
+#                               get("var_stat") == "Porcentaje promedio acumulado"),
+#        aes_string(x = "eda", y = "value", group = "p14", color = "p14")) +
+#   geom_line() +
+#   geom_point() +
+#   labs(title = "title", y = paste0("Porcentaje","promedio acumulado"), x = "Edad", 
+#        caption = "Fuente: Elaboración propia - ENCO Julio 2022", color = "") +
+#   scale_y_continuous(limits=c(0,1), breaks = seq(0,1,0.25), labels = scales::percent) +
+#   facet_wrap(~var_stat) +
+#   theme_bw() +
+#   theme(legend.position = "top") +
+#   guides(color = guide_legend(reverse=TRUE))
+
+# 2.- Función general para visualización sencilla y wrappeada
+
+plot_meanline <- function(colname, title, compara){
   db <- make_data(clean_cuestbas, colname)
   
- plot <- ggplot(db %>% filter(across(c(starts_with(colname)))), 
-           aes_string(x = "eda", y = "cummean", group = colname, color = colname)) +
+  if(compara == TRUE){
+    wrapvars <- c("Porcentaje", "Porcentaje promedio acumulado")
+    ifelse(!dir.exists(file.path("/Users/adrix-lg/Repos/blog-cheyenne/grafs/prom-compare")),
+           dir.create(file.path("/Users/adrix-lg/Repos/blog-cheyenne/grafs/prom-compare")), 
+           FALSE)
+    savingdir <- "prom-compare/"
+  } else{
+    wrapvars <- c("Porcentaje promedio acumulado")
+    ifelse(!dir.exists(file.path("/Users/adrix-lg/Repos/blog-cheyenne/grafs/prom-acum")),
+           dir.create(file.path("/Users/adrix-lg/Repos/blog-cheyenne/grafs/prom-acum")), 
+           FALSE)
+    savingdir <- "prom-acum/"
+  }
+  
+ plot <- ggplot(db %>% filter(get(colname) != "No sabe",
+                              var_stat %in% wrapvars), 
+           aes_string(x = "eda", y = "value", group = colname, color = colname)) +
       geom_line() +
       geom_point() +
-      labs(title = title, y = "Promedio acumulado", x = "Edad", 
+      labs(title = title, y = "", x = "Edad", 
            caption = "Fuente: Elaboración propia - ENCO Julio 2022", color = "") +
-      scale_y_continuous(limits=c(0,1), breaks = seq(0,1,0.25)) +
+      scale_y_continuous(limits=c(0,1), breaks = seq(0,1,0.25), labels = scales::percent) +
+      facet_wrap(~var_stat) +
       theme_bw() +
       theme(legend.position = "top") +
       guides(color = guide_legend(reverse=TRUE))
   
- plot
+ # print(plot)
  
-  ggsave(paste0("/Users/adrix-lg/Repos/blog-cheyenne/grafs/", colname, ".png"), plot,
+  ggsave(paste0("/Users/adrix-lg/Repos/blog-cheyenne/grafs/", savingdir,colname, ".png"), plot,
          width = 15, height = 9)
 }
 
 # Muestra 
-plot_meanline("p14", "¿Algún miembro de este hogar o usted están planeando comprar un automóvil nuevo o usado
-en los próximos 2 años?")
+# plot_meanline("p14", "¿Algún miembro de este hogar o usted están planeando comprar un automóvil nuevo o usado
+# en los próximos 2 años?", compara = TRUE)
 
 # Loop
 preguntas <- paste0("p", c(1:15))
@@ -105,7 +242,8 @@ titulos <- c("¿Cómo describiría usted su situación económica comparada con 
 
 pb <- txtProgressBar(min = 1, max = length(preguntas), style = 3)
 for (i in 1:length(preguntas)){
-  plot_meanline(preguntas[i], titulos[i])
+  plot_meanline(preguntas[i], titulos[i], compara = TRUE)
+  plot_meanline(preguntas[i], titulos[i], compara = FALSE)
   
   setTxtProgressBar(pb, i)
 }
