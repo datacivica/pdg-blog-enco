29e51716526b3f1516f70b60f9b43de423ee294d
adrian-DC <adrian.lara@datacivica.org>
2022-10-19 17:09:25 -0500

    ajustes post revisión caro

--- src/grafs-blog.R
+++ src/grafs-blog.R
@@ -31,11 +31,13 @@ loadfonts(quiet = T)
 adrix_theme <- theme_bw() +
   theme(legend.position = "top", 
         text = element_text(size = 15, family = "Roboto Slab"),
-        plot.title = element_text(size = 24, hjust = 0.5), 
-        plot.subtitle = element_text(hjust = 0.5),
+        plot.title = element_text(size = 40, hjust = 0.5), 
+        plot.subtitle = element_text(hjust = 0.5, size = 30),
+        plot.caption = element_text(size = 20, hjust = 0),
+        axis.title = element_text(size = 24),
         legend.text = element_text(size = 18), 
         strip.text.x = element_text(size = 19),
-        axis.text.x = element_text(size=12.5)) 
+        axis.text = element_text(size = 22)) 
 
 # Títulos
 titulos <- c(
@@ -109,7 +111,7 @@ mk_data_noage <- function(db, inciso){
 
 clean_cuestbas <- readRDS(files$clean_cuestbas) 
 
-# Graf General 
+# Graf aspiraciones materiales global 
 tempo <- mk_data_noage(clean_cuestbas, "p14") %>% 
   bind_rows(mk_data_noage(clean_cuestbas, "p9")) %>%
   bind_rows(mk_data_noage(clean_cuestbas, "p15")) %>% 
@@ -125,20 +127,21 @@ ggplot(tempo, aes(x = respuesta, y = mean, fill = respuesta)) +
   geom_errorbar(aes(ymin = lower_ic, ymax = upper_ic), width=.2,
                 position=position_dodge(.9)) +
   geom_text(aes(label = paste0(c(as.character(round(mean, digits = 2)*100)), "%")),
-            family = "Roboto Slab", hjust = -0.95, vjust = -0.2, color =  "black") +
+            family = "Roboto Slab", hjust = -0.75, vjust = -0.2, color =  "black", size = 10) +
   scale_fill_manual(values = c("#9BE8E6", "#CFA6F2")) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
   facet_grid(~pregunta) +
+  coord_cartesian(ylim = c(0, 0.95)) +
   labs(title =  "¿Alguien en este hogar o usted están planeando...?",
-       subtitle = "Porcentajes con intervalos de confianza", y = "Porcentaje", x = "", 
+       subtitle = "Porcentajes con intervalos de confianza", y = "", x = "", 
        caption = "Fuente: Elaboración propia - ENCO Septiembre 2022", color = "", fill = "") +
   adrix_theme +
   theme(legend.position = "none",
-        strip.text.x = element_text(size = 10))
+        strip.text.x = element_text(size = 18))
 
 ggsave(files$graf_aspiraciones_general, width = 16, height = 10)
 
-# Graf aspiraciones
+# Graf aspiraciones materiales por edad
   
 tempo <- mk_data_intervalos(clean_cuestbas, "p14")  %>% 
   bind_rows(mk_data_intervalos(clean_cuestbas, "p9")) %>%
@@ -159,14 +162,23 @@ ggplot(tempo, aes(x = grupo_edad, y = mean, group = respuesta,
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
   scale_color_manual(values = c("aquamarine3", "blueviolet", "coral")) +
   scale_fill_manual(values = c("aquamarine3", "blueviolet", "coral")) +
+  scale_x_discrete(guide = guide_axis(n.dodge=2))+
   facet_wrap(~ pregunta) +
+  coord_cartesian(ylim = c(0, 1.05)) +
   labs(title =  "¿Qué porcentaje de personas en México piensan que\n alguien de su hogar o ellas mismas podrán...?",
        subtitle = "Porcentajes por grupo edad (con intervalos de confianza)", y = "", x = "Grupo de edad", 
        caption = "Fuente: Elaboración propia - ENCO Septiembre 2022") +
   adrix_theme +
-  guides(fill = guide_legend(reverse = TRUE), color = guide_legend(reverse = TRUE)) +
-  theme(strip.text.x = element_text(size = 16),
-        legend.title = element_blank())
+  guides(fill = guide_legend(reverse = TRUE), 
+         color = guide_legend(reverse = TRUE)) +
+  theme(legend.title = element_blank(),
+        legend.text = element_text(size = 22),
+        legend.position = "top",
+        plot.title = element_text(size = 35),
+        plot.subtitle = element_text(margin = unit(c(0.5,0,0.75,0), "cm")),
+        axis.text.x = element_text(size = 15),
+        axis.text.y = element_text(size = 15),
+        axis.title.x = element_text(margin = unit(c(0.75,0,0.5,0), "cm")))
   
 ggsave(files$graf_aspiraciones_edades, width = 16, height = 10)
 
@@ -178,40 +190,24 @@ ggplot(tempo, aes(x = grupo_edad, y = mean, group = pregunta, color = pregunta, 
   geom_line(size = 1.4) +
   geom_point(size = 2.6) +
   geom_text(aes(label = paste0(c(as.character(round(mean, digits = 2)*100)), "%")),
-            family = "Roboto Slab", size = 6, vjust = -1.2, color =  "black") +
+            family = "Roboto Slab", size = 8, vjust = -1.2, color =  "black") +
   geom_ribbon(aes(ymin = lower_ic,ymax = upper_ic), alpha=0.1) +
   scale_color_manual(values = c("aquamarine3")) +
   scale_fill_manual(values = c("aquamarine3")) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
-  labs(title =  "¿Qué porcentaje de personas en México afirma poder ahorrar una parte de sus ingresos?",
+  labs(title =  "¿Qué porcentaje de personas en México afirma\npoder ahorrar una parte de sus ingresos?",
        subtitle = "Porcentajes por grupo edad (con intervalos de confianza)", y = "", x = "Grupo de edad", 
        caption = "Fuente: Elaboración propia - ENCO Septiembre 2022") +
   adrix_theme +
-  theme(legend.position = "none")
+  theme(legend.position = "none",
+        plot.title = element_text(size = 37),
+        plot.subtitle = element_text(margin = unit(c(0.5,0,0.75,0), "cm")),
+        axis.title.x = element_text(margin = unit(c(0.75,0,0.5,0), "cm")))
 
 ggsave(files$graf_ahorro, width = 16, height = 10)
 
 
 # Grafs expectativas global
-tempo <- mk_data_noage(clean_cuestbas, "p2")  %>% 
-  bind_rows(mk_data_noage(clean_cuestbas, "p4")) %>% 
-  bind_rows(mk_data_noage(clean_cuestbas, "p6")) %>% 
-  filter(respuesta != "No sabe") %>% 
-  mutate(pregunta = case_when(
-    pregunta == "p2" ~ "Su situación económica personal",
-    pregunta == "p4" ~ "La situación económica de los otros miembros de su hogar",
-    pregunta == "p6" ~ "La situación económica del país"),
-    respuesta = case_when(
-      respuesta == "Peor o mucho peor" ~ "Será peor o mucho peor",
-      respuesta == "Mejor o mucho mejor" ~ "Será mejor o mucho mejor",
-      respuesta == "Igual" ~ "Será igual")) %>% 
-  mutate(respuesta = factor(respuesta, levels = c( "Será mejor o mucho mejor","Será peor o mucho peor", "Será igual")),
-         pregunta = factor(pregunta, levels = c("Su situación económica personal",
-                                                "La situación económica de los otros miembros de su hogar", 
-                                                "La situación económica del país")))
-
-
-# Grafs expectativas
 
 tempo <- mk_data_intervalos(clean_cuestbas, "p2")  %>% 
   bind_rows(mk_data_intervalos(clean_cuestbas, "p4")) %>% 
@@ -219,7 +215,7 @@ tempo <- mk_data_intervalos(clean_cuestbas, "p2")  %>%
   filter(respuesta != "No sabe") %>% 
   mutate(pregunta = case_when(
            pregunta == "p2" ~ "Su situación económica personal",
-           pregunta == "p4" ~ "La situación económica de los otros miembros de su hogar",
+           pregunta == "p4" ~ "La situación económica de los\notros miembros de su hogar",
            pregunta == "p6" ~ "La situación económica del país"),
          respuesta = case_when(
            respuesta == "Peor o mucho peor" ~ "Será peor o mucho peor",
@@ -227,7 +223,7 @@ tempo <- mk_data_intervalos(clean_cuestbas, "p2")  %>%
            respuesta == "Igual" ~ "Será igual")) %>% 
   mutate(respuesta = factor(respuesta, levels = c("Será peor o mucho peor", "Será igual", "Será mejor o mucho mejor")),
          pregunta = factor(pregunta, levels = c("Su situación económica personal",
-                                                "La situación económica de los otros miembros de su hogar", 
+                                                "La situación económica de los\notros miembros de su hogar", 
                                                 "La situación económica del país")))
 
 
@@ -236,6 +232,7 @@ ggplot(tempo, aes(x = grupo_edad, y = mean, group = respuesta, fill = respuesta)
   geom_text(aes(label = paste0(c(as.character(round(mean, digits = 2)*100)), "%")),
                   family = "Roboto Slab", color =  "white", size = 6, position = position_stack(vjust = 0.5)) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  scale_x_discrete(guide = guide_axis(n.dodge=2))+
   facet_wrap(~ pregunta, ncol = 3) +
   scale_fill_manual(values = c("blueviolet", "#CFA6F2", "aquamarine3")) +
   labs(title =  "En un año, ¿Cómo cree que será..?",
@@ -243,10 +240,11 @@ ggplot(tempo, aes(x = grupo_edad, y = mean, group = respuesta, fill = respuesta)
        caption = "Fuente: Elaboración propia - ENCO Septiembre 2022") +
   adrix_theme +
   guides(fill = guide_legend(reverse = TRUE)) +
-  theme(strip.text.x = element_text(size = 16),
-        legend.position = "bottom",
+  theme(legend.position = "top",
         legend.title = element_blank(),
-        legend.text = element_text(size = 16))
+        axis.text.x = element_text(size = 15),
+        plot.subtitle = element_text(margin = unit(c(0.5,0,0.75,0), "cm")),
+        axis.title.x = element_text(margin = unit(c(0.75,0,0.5,0), "cm")))
 
 ggsave(files$graf_expectativas, width = 16, height = 10)
 
@@ -256,25 +254,31 @@ tempo <- mk_data_noage(clean_cuestbas %>% filter(p14 == "No"), "p10")  %>%
   bind_rows(., mk_data_noage(clean_cuestbas %>% filter(p15 == "No"), "p10")%>%  mutate(p ="p15")) %>%
   bind_rows(., mk_data_noage(clean_cuestbas %>% filter(p9 == "No"), "p10") %>%  mutate(p="p9")) %>% 
   bind_rows(., mk_data_noage(clean_cuestbas, "p10") %>% mutate(p="p10")) %>% 
-  filter(respuesta %in% c("Sí", "No"))  %>% 
+  filter(respuesta %in% c("Sí"))  %>% 
   mutate(etiqueta = factor(p,
                            levels = c("p10", "p14", "p15", "p9"),
-                           labels = c("Población en general", "Personas que no piensan comprar un coche",
-                                      "Personas que no piensan comprar, remodelar o construir una casa",
-                                      "Personas que no piensan salir de vacaciones")))
+                           labels = c("Población en general", "Personas que no piensan\ncomprar un coche",
+                                      "Personas que no piensan\n comprar, remodelar o\n construir una casa",
+                                      "Personas que no piensan\nsalir de vacaciones")))
 
 
-ggplot(tempo, aes(x = fct_rev(etiqueta), y = mean, group = respuesta, fill = respuesta)) +
-  geom_col(position = "dodge") +
+ggplot(tempo, aes(x = fct_rev(etiqueta), y = mean)) +
+  geom_col(position = "dodge", fill = "#CFA6F2") +
   geom_text(aes(label = paste0(c(as.character(round(mean, digits = 2)*100)), "%")),
-            family = "Roboto Slab", size = 6, color =  "black",
+            family = "Roboto Slab", size = 7, color =  "black",
             position = position_dodge(width = 1), hjust = -.25) +
-  coord_flip(ylim = c(0, 0.90)) +
   scale_fill_manual(values = c("#9BE8E6", "#CFA6F2")) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  # scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
+  coord_flip(ylim = c(0, 0.35)) +
   labs(title =  "¿Qué porcentaje de personas en México afirma\npoder ahorrar una parte de sus ingresos?",
        subtitle = "Porcentajes por tipo de expectativas", y = "", x = "", 
        caption = "Fuente: Elaboración propia - ENCO Septiembre 2022") +
-  adrix_theme 
+  adrix_theme +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  theme(plot.title = element_text(size = 30),
+        plot.subtitle = element_text(size = 26, margin = unit(c(0.25,0,0,25,0), "cm")),
+        legend.title = element_blank(),
+        axis.text.x = element_text(size = 12))
   
 ggsave(files$graf_ahorro_tipos, width = 16, height = 10)
